5e40854a314957647462305a39652f2ac4212828
Dominik Šramko <dominiksramko@gmail.com>
2016-03-24 04:20:27 +0100

    i3 config rebrand

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Mouse settings
 xset m 0 0 &
 xset s 0 0 &
+
+dropbox &
---
+++ i3/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# For testing
+cp themes/ice.theme ~/.Xdefaults
+
+cp i3status.conf ~/.i3status.conf
+cp i3config ~/.config/i3/config
+
+cp autostart.sh ~/.autostart
+chmod +x ~/.autostart
