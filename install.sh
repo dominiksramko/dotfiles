4513a81b29ce0c32afe5bc9cd13b3df019c835cd
Dominik Šramko <dominiksramko@gmail.com>
2016-05-14 03:06:55 +0200

    God damn it

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp .Xresources ~/.Xresources
 xrdb ~/.Xresources
 
 cp i3/config ~/.config/i3/config
-cp i3/.i3status.conf ~/.i3status.conf
+cp i3/i3status.conf ~/.i3status.conf
 
 cp autostart.sh ~/.autostart
 chmod +x ~/.autostart
